73472fc96a4162b097e2eb01f2e767fc8c2f6a81
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-16 10:20:34 +0700

    quiz

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -12,4 +12,27 @@ interface PostResult {
   title: string;
 }
 
-export function getPostsByUser() {}
+export const getPostsByUser = async (userId: number): Promise<PostResult[]> => {
+  try {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const { data } = await axios.get<ApiResult[]>(url);
+    if (!data || data.length === 0) {
+      return [];
+    }
+    const result: PostResult[] = [];
+    for (const post of data){
+        result.push({
+          id: post.id,
+          title: post.title
+        });
+    }
+    return result;
+
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Failed : ${error.message}`);
+    } else {
+      throw new Error('Unkown Error');
+    }
+  }
+}
