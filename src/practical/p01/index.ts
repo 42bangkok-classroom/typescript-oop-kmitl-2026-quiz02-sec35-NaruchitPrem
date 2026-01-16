5ffdc3440294db3407b1d6281a0ca0bd7deaf549
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-16 10:03:10 +0700

    quiz

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -12,4 +12,35 @@ interface PostResult {
   title: string;
 }
 
-export function getEdgePosts() {}
+export const getEdgePosts = async (): Promise<PostResult[]> => {
+  try {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const { data } = await axios.get<ApiResult[]>(url);
+    if (!data || data.length === 0) {
+      return [];
+    }
+    const firstPost = data[0];
+    const lastPost = data[data.length - 1];
+    
+    const result: PostResult[] = [];
+
+    result.push({
+      id: firstPost.id,
+      title: firstPost.title
+    });
+
+    result.push({
+      id: lastPost.id,
+      title: lastPost.title
+    });
+
+    return result;
+
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(`Failed : ${error.message}`);
+    } else {
+      throw new Error('Unkown Error');
+    }
+  }
+}
