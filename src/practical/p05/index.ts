ac3703af0d520b407fb645cc4922605ecfe877ed
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-16 10:38:25 +0700

    quiz

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,2 +1,29 @@
-export function safeFetchComment() {}
+import axios from 'axios';
 
+interface ApiComment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+interface CommentResult {
+  id: number;
+  body: string;
+}
+
+export const safeFetchComment = async (commentId: number): Promise<CommentResult | null> => {
+  try {
+    const url = `https://jsonplaceholder.typicode.com/comments/${commentId}`;
+    const { data } = await axios.get<ApiComment>(url);
+
+    return {
+      id: data.id,
+      body: data.body
+    };
+
+  } catch (error) {
+    return null;
+  }
+};
