807661f6c27a4010183cd2a1f630cb8f01da7983
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-01-16 09:48:34 +0700

    quiz

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
+interface ApiResult {
+  userId : number;
+  id : number;
+  title: string;
+  body: string;
+}
+
+interface PostResult {
+  id: number;
+  title: string;
+}
+
 export function getEdgePosts() {}
